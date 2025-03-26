b85411abcae7d524ae09ba695ee4cfcafa905d88
mattriley619 <76889891+mattriley619@users.noreply.github.com>
2025-03-26 10:02:56 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<html>
+<body>
+
+<form action="poll.php" method="POST">
+    Username: <input type="text" name="username" id="username"><br>
+    <input type="submit">
+</form>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+<?php
---
+++ poll.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+
+<?php
+
+$username = $_POST["username"];
+echo username;
+?><br>
+
+Favorite Color?:
+<input type="radio" name="color" value="red">Red
+<input type="radio" name="color" value="blue">Blue
+<input type="radio" name="color" value="green">Green
+
+</body>
+</html><?php
